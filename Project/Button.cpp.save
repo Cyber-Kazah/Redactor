b8a57a64cf0437506173fc82ab239776a3e41f14
Cyber-Kazah <116567401+Cyber-Kazah@users.noreply.github.com>
2023-12-15 18:48:40 +0400

    Add files via upload

--- Project/Button.cpp.save
+++ Project/Button.cpp.save
@@ -6,6 +6,7 @@
 struct Button
 {
     int x;
+    int y;
     const char* name;
     string category;
 
@@ -13,19 +14,19 @@ struct Button
     {
         txSetColor(TX_TRANSPARENT);
         txSetFillColor(TX_GRAY);
-        Win32::RoundRect (txDC(), x+5, 35, x+145, 75, 30, 30);
+        Win32::RoundRect (txDC(), x+5, y+5, x+145, y+45, 30, 30);
         txSetColor(TX_BLACK, 2);
         txSetFillColor(TX_WHITE);
-        Win32::RoundRect (txDC(), x, 30, x+140, 70, 30, 30);
+        Win32::RoundRect (txDC(), x, y, x+140, y+40, 30, 30);
         txSetColor(TX_BLACK);
         txSelectFont("Times New Roman", 24);
-        txDrawText (x, 30, x+140, 70, name);
+        txDrawText (x, y, x+140, y+40, name);
     }
     bool click()
     {
         return( txMouseButtons() == 1 &&
         txMouseX() >= x && txMouseX() <= x+140 &&
-        txMouseY() >= 35 && txMouseY() <= 75);
+        txMouseY() >= y && txMouseY() <= y+40);
     }
 
 
--- Project/main.cpp
+++ Project/main.cpp
@@ -6,6 +6,8 @@
     #include <stdio.h>
     #include <dirent.h>
 
+    using namespace std;
+
     int get_w(string adress)
     {
 
@@ -66,23 +68,204 @@
       return count_pic;
     }
 
+    string runFiledialog(bool isSave)
+    {
+
+            string Filename = "";
+            OPENFILENAME ofn;
+            TCHAR szFile[260] = {0};       //размеры хранилища
+
+            // Initialize OPENFILENAME
+            ZeroMemory(&ofn, sizeof(ofn));
+            ofn.lStructSize = sizeof(ofn);
+            ofn.hwndOwner = txWindow();
+            ofn.lpstrFile = szFile;
+            ofn.nMaxFile = sizeof(szFile);
+            ofn.lpstrFilter = ("Text\0*.TXT\0");
+            ofn.nFilterIndex = 1;
+            ofn.lpstrFileTitle = NULL;
+            ofn.nMaxFileTitle = 0;
+            ofn.lpstrInitialDir = NULL;
+            ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST;
+
+            // Display the Open dialog box.
+            if(isSave)
+            {
+                if (GetSaveFileName(&ofn)==TRUE)
+                {
+                    Filename = ofn.lpstrFile;
+                    Filename = Filename + ".txt";
+                }
+            }
+            else
+            {
+                if (GetOpenFileName(&ofn)==TRUE)
+                {
+                    Filename = ofn.lpstrFile;
+                }
+            }
+
+
+
+        return Filename;
+    }
+
+    inline int GetFilePointer(HANDLE FileHandle)
+    {
+    return SetFilePointer(FileHandle, 0, 0, FILE_CURRENT);
+}
+
+    bool SaveBMPFile(char *filename, HBITMAP bitmap, HDC bitmapDC, int width, int height)
+    {
+        bool Success=0;
+        HBITMAP OffscrBmp=NULL;
+        HDC OffscrDC=NULL;
+        LPBITMAPINFO lpbi=NULL;
+        LPVOID lpvBits=NULL;
+        HANDLE BmpFile=INVALID_HANDLE_VALUE;
+        BITMAPFILEHEADER bmfh;
+        if ((OffscrBmp = CreateCompatibleBitmap(bitmapDC, width, height)) == NULL)
+            return 0;
+        if ((OffscrDC = CreateCompatibleDC(bitmapDC)) == NULL)
+            return 0;
+        HBITMAP OldBmp = (HBITMAP)SelectObject(OffscrDC, OffscrBmp);
+        BitBlt(OffscrDC, 0, 0, width, height, bitmapDC, 0, 0, SRCCOPY);
+        if ((lpbi = (LPBITMAPINFO)(new char[sizeof(BITMAPINFOHEADER) + 256 * sizeof(RGBQUAD)])) == NULL)
+            return 0;
+        ZeroMemory(&lpbi->bmiHeader, sizeof(BITMAPINFOHEADER));
+        lpbi->bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+        SelectObject(OffscrDC, OldBmp);
+        if (!GetDIBits(OffscrDC, OffscrBmp, 0, height, NULL, lpbi, DIB_RGB_COLORS))
+            return 0;
+        if ((lpvBits = new char[lpbi->bmiHeader.biSizeImage]) == NULL)
+            return 0;
+        if (!GetDIBits(OffscrDC, OffscrBmp, 0, height, lpvBits, lpbi, DIB_RGB_COLORS))
+            return 0;
+        if ((BmpFile = CreateFile(filename,
+                            GENERIC_WRITE,
+                            0, NULL,
+                            CREATE_ALWAYS,
+                            FILE_ATTRIBUTE_NORMAL,
+                            NULL)) == INVALID_HANDLE_VALUE)
+            return 0;
+        DWORD Written;
+        bmfh.bfType = 19778;
+        bmfh.bfReserved1 = bmfh.bfReserved2 = 0;
+        if (!WriteFile(BmpFile, &bmfh, sizeof(bmfh), &Written, NULL))
+            return 0;
+        if (Written < sizeof(bmfh))
+            return 0;
+        if (!WriteFile(BmpFile, &lpbi->bmiHeader, sizeof(BITMAPINFOHEADER), &Written, NULL))
+            return 0;
+        if (Written < sizeof(BITMAPINFOHEADER))
+            return 0;
+        int PalEntries;
+        if (lpbi->bmiHeader.biCompression == BI_BITFIELDS)
+            PalEntries = 3;
+        else PalEntries = (lpbi->bmiHeader.biBitCount <= 8) ?
+                          (int)(1 << lpbi->bmiHeader.biBitCount) : 0;
+        if(lpbi->bmiHeader.biClrUsed)
+        PalEntries = lpbi->bmiHeader.biClrUsed;
+        if(PalEntries){
+        if (!WriteFile(BmpFile, &lpbi->bmiColors, PalEntries * sizeof(RGBQUAD), &Written, NULL))
+            return 0;
+            if (Written < PalEntries * sizeof(RGBQUAD))
+                return 0;
+        }
+        bmfh.bfOffBits = GetFilePointer(BmpFile);
+        if (!WriteFile(BmpFile, lpvBits, lpbi->bmiHeader.biSizeImage, &Written, NULL))
+            return 0;
+        if (Written < lpbi->bmiHeader.biSizeImage)
+            return 0;
+        bmfh.bfSize = GetFilePointer(BmpFile);
+        SetFilePointer(BmpFile, 0, 0, FILE_BEGIN);
+        if (!WriteFile(BmpFile, &bmfh, sizeof(bmfh), &Written, NULL))
+            return 0;
+        if (Written < sizeof(bmfh))
+            return 0;
+
+        CloseHandle (BmpFile);
+
+        delete [] (char*)lpvBits;
+        delete [] lpbi;
+
+        DeleteDC (OffscrDC);
+        DeleteObject (OffscrBmp);
+
+
+        return 1;
+    }
+
+    bool ScreenCapture(int x, int y, int width, int height, char *filename, HWND hwnd)
+    {
+    HDC hDC = GetDC(hwnd);
+    HDC hDc = CreateCompatibleDC(hDC);
+
+    HBITMAP hBmp = CreateCompatibleBitmap(hDC, width, height);
+
+    HGDIOBJ old= SelectObject(hDc, hBmp);
+    BitBlt(hDc, 0, 0, width, height, hDC, x, y, SRCCOPY);
+
+    bool ret = SaveBMPFile(filename, hBmp, hDc, width, height);
+
+    SelectObject(hDc, old);
+
+    DeleteObject(hBmp);
+
+    DeleteDC (hDc);
+    ReleaseDC (hwnd, hDC);
+
+    return ret;
+}
+
+
+
+    //кол. кнопок
+    const int count_btn = 12;
+    //сохранить загрузить
+    const int btn_prntsc = count_btn-6;
+    //сохранить и загрузить
+    const int btn_save = count_btn-5;
+    const int btn_load = count_btn-4;
+    //справка
+    const int btn_help = count_btn-3;
+    //справка
+    const int btn_exit = count_btn-2;
+    //справка
+    const int btn_menu = count_btn-1;
+    //меню
+    const int page_menu = 0;
+    //Редактор
+    const int page_redactor = 1;
+    //Справка
+    const int page_help = 2;
+
+
+
     int main()
     {
     txCreateWindow (1300, 700);
     txDisableAutoPause();
     txTextCursor (false);
-    int count_btn = 6;
     int count_pic = 0;
     int nCentralPic = 0;
 
+    int page = page_menu;
+
     //кнопки масив(маленькие слова +120, большие +200)
     Button btn[count_btn];
-    btn[0] = {60, "персонажи", "персонажи"};
-    btn[1] = {260, "растительность", "растительность"};
-    btn[2] = {460, "шляпы", "шляпы"};
-    btn[3] = {660, "оптика", "оптика"};
-    btn[4] = {860, "аксессуары", "аксессуары"};
-    btn[5] = {1060, "друзья", "друзья"};
+    btn[0] = {60, 30, "персонажи", "персонажи"};
+    btn[1] = {260, 30, "растительность", "растительность"};
+    btn[2] = {460, 30,  "шляпы", "шляпы"};
+    btn[3] = {660, 30,  "оптика", "оптика"};
+    btn[4] = {860, 30,  "аксессуары", "аксессуары"};
+    btn[5] = {1060, 30,  "друзья", "друзья"};
+    btn[6] = {760, 650,  "Снимок экрана", ""};
+    btn[7] = {60, 650,  "Сохранить", ""};
+    btn[8] = {210, 650,  "Загрузить", ""};
+    btn[9] = {360, 650,  "Справка", ""};
+    btn[10] = {960, 650,  "Выход", ""};
+    btn[11] = {560, 270,  "Старт", ""};
 
     //картинки меню выбора(масив)
     Picture menuPic[100];
@@ -119,137 +302,270 @@
     int vybor = -1;
     bool mouse_click = false;
 
-    while(!GetAsyncKeyState (VK_ESCAPE))
+    while(!btn[btn_exit].click())
     {
         txBegin();
         txSetColor(TX_WHITE);
         txSetFillColor(TX_WHITE);
         txClear();
-        //кнопки
-        for(int i=0; i<count_btn; i++)
+
+        //меню
+        if(page == page_menu)
         {
-            btn[i].draw();
-        }
-        //рисование картинок в меню
-        for(int i=0; i<count_pic; i++)
-        {
-            menuPic[i].draw();
-        }
-        //рисование картинок на поле
-        for(int i=0; i<nCentralPic; i++)
-        {
-            centrPic[i].draw();
+            btn[btn_menu].draw();
+            if(btn[btn_menu].click())
+            {
+                page = page_redactor;
+                txSleep(100);
+            }
         }
 
-        for(int ib=0; ib<count_btn; ib++)
+
+        //редактор
+        if(page == page_redactor)
         {
-            if(btn[ib].click())
+            txSetColor(TX_BLACK);
+            txSetFillColor(TX_WHITE);
+            txRectangle(250, 100, 950, 600);
+
+            //кнопки
+            for(int i=0; i<count_btn-1; i++)
             {
-                for(int ip=0; ip<count_pic; ip++)
+                btn[i].draw();
+            }
+            //рисование картинок в меню
+            for(int i=0; i<count_pic; i++)
+            {
+                menuPic[i].draw();
+            }
+            //рисование картинок на поле
+            for(int i=0; i<nCentralPic; i++)
+            {
+                centrPic[i].draw();
+            }
+
+            for(int ib=0; ib<count_btn; ib++)
+            {
+                if(btn[ib].click())
                 {
-                    menuPic[ip].visible = false;
-                    if(menuPic[ip].category == btn[ib].category)
+                    for(int ip=0; ip<count_pic; ip++)
                     {
-                        menuPic[ip].visible = true;
+                        menuPic[ip].visible = false;
+                        if(menuPic[ip].category == btn[ib].category)
+                        {
+                            menuPic[ip].visible = true;
+                        }
                     }
                 }
             }
-        }
 
-        for(int npic=0; npic<count_pic; npic++)
-        {
-            if(menuPic[npic].click() && menuPic[npic].visible)
+            for(int npic=0; npic<count_pic; npic++)
             {
-                 while(txMouseButtons() == 1)
-                 {
-                    txSleep(10);
-                 }
-                 centrPic[nCentralPic] = {  500,
-                                            100,
-                                            menuPic[npic].adress,
-                                            menuPic[npic].pic,
-                                            menuPic[npic].w,
-                                            menuPic[npic].h,
-                                            menuPic[npic].w,
-                                            menuPic[npic].h,
-                                            menuPic[npic].visible,
-                                            menuPic[npic].category};
-                 nCentralPic ++;
-            }
-
-
-         }
-
-        for(int i=0; i<nCentralPic; i++)
-        {
-              if(centrPic[i].click() && centrPic[i].visible)
-              {
-                    vybor = i;
-                    mouse_click = false;
-              }
-        }
-
-        if(vybor>=0 && GetAsyncKeyState (VK_DELETE))
-        {
-                centrPic[vybor] = centrPic[nCentralPic-1];
-                nCentralPic--;
-                vybor = -1;
-                mouse_click = true;
-        }
-
-
-
-
-        if(vybor>=0)
-        {
-            if(GetAsyncKeyState (VK_LEFT))
-             {
-                centrPic[vybor].x -= 5;
-             }
-            if(GetAsyncKeyState (VK_RIGHT))
-             {
-                centrPic[vybor].x += 5;
-             }
-            if(GetAsyncKeyState (VK_DOWN))
-             {
-                centrPic[vybor].y += 5;
-             }
-            if(GetAsyncKeyState (VK_UP))
-             {
-                centrPic[vybor].y -= 5;
-             }
-            if(GetAsyncKeyState (VK_OEM_PLUS) || GetAsyncKeyState (VK_ADD))
-             {
-                centrPic[vybor].w_scr = centrPic[vybor].w_scr * 1.1;
-                centrPic[vybor].h_scr = centrPic[vybor].h_scr * 1.1;
-             }
-            if(GetAsyncKeyState (VK_OEM_MINUS) || GetAsyncKeyState (VK_SUBTRACT))
-             {
-                centrPic[vybor].w_scr = centrPic[vybor].w_scr * 0.9;
-                centrPic[vybor].h_scr = centrPic[vybor].h_scr * 0.9;
-             }
-        }
-
-        if(vybor>=0)
-        {
-            if(txMouseButtons() == 1 && !mouse_click)
-            {
-                centrPic[vybor].x = txMouseX() - centrPic[vybor].w_scr/2;
-                centrPic[vybor].y = txMouseY() - centrPic[vybor].h_scr/2;
-            }
-            else
-            {
-                if(txMouseButtons() != 1)
+                if(menuPic[npic].click() && menuPic[npic].visible)
                 {
-                    mouse_click = true;
+                     while(txMouseButtons() == 1)
+                     {
+                        txSleep(10);
+                     }
+                     centrPic[nCentralPic] = {  500,
+                                                100,
+                                                menuPic[npic].adress,
+                                                menuPic[npic].pic,
+                                                menuPic[npic].w,
+                                                menuPic[npic].h,
+                                                menuPic[npic].w,
+                                                menuPic[npic].h,
+                                                menuPic[npic].visible,
+                                                menuPic[npic].category};
+                     nCentralPic ++;
                 }
+
+
             }
 
+            for(int i=0; i<nCentralPic; i++)
+            {
+                  if(centrPic[i].click() && centrPic[i].visible)
+                  {
+                        vybor = i;
+                        mouse_click = false;
+                  }
+            }
+
+            if(vybor>=0 && GetAsyncKeyState (VK_DELETE))
+            {
+                    centrPic[vybor] = centrPic[nCentralPic-1];
+                    nCentralPic--;
+                    vybor = -1;
+                    mouse_click = true;
+            }
+            //передвижение
+            if(vybor>=0)
+            {
+                if(GetAsyncKeyState (VK_LEFT))
+                 {
+                    centrPic[vybor].x -= 5;
+                 }
+                if(GetAsyncKeyState (VK_RIGHT))
+                 {
+                    centrPic[vybor].x += 5;
+                 }
+                if(GetAsyncKeyState (VK_DOWN))
+                 {
+                    centrPic[vybor].y += 5;
+                 }
+                if(GetAsyncKeyState (VK_UP))
+                 {
+                    centrPic[vybor].y -= 5;
+                 }
+                if(GetAsyncKeyState (VK_OEM_PLUS) || GetAsyncKeyState (VK_ADD))
+                 {
+                    centrPic[vybor].w_scr = centrPic[vybor].w_scr * 1.1;
+                    centrPic[vybor].h_scr = centrPic[vybor].h_scr * 1.1;
+                 }
+                if(GetAsyncKeyState (VK_OEM_MINUS) || GetAsyncKeyState (VK_SUBTRACT))
+                 {
+                    centrPic[vybor].w_scr = centrPic[vybor].w_scr * 0.9;
+                    centrPic[vybor].h_scr = centrPic[vybor].h_scr * 0.9;
+                 }
+            }
+
+            if(vybor>=0)
+            {
+                if(txMouseButtons() == 1 && !mouse_click)
+                {
+                    centrPic[vybor].x = txMouseX() - centrPic[vybor].w_scr/2;
+                    centrPic[vybor].y = txMouseY() - centrPic[vybor].h_scr/2;
+                }
+                else
+                {
+                    if(txMouseButtons() != 1)
+                    {
+                        mouse_click = true;
+                    }
+                }
+
+            }
+            //сохранения
+            if(btn[btn_save].click())
+            {
+                string FileName = runFiledialog(true);
+                ofstream fileout;
+                fileout.open(FileName);
+                if (fileout.is_open())
+                {
+                    for(int i=0; i<nCentralPic; i++)
+                    {
+                        if(centrPic[i].visible)
+                        {
+                            fileout << centrPic[i].x << endl;
+                            fileout << centrPic[i].y << endl;
+                            fileout << centrPic[i].adress << endl;
+                            fileout << centrPic[i].w_scr << endl;
+                            fileout << centrPic[i].h_scr << endl;
+                        }
+                    }
+                }
+                fileout.close();
+            }
+
+            //загрузка
+            if(btn[btn_load].click())
+            {
+                string FileName = runFiledialog(false);
+                for(int i = 0; i<count_pic; i++)
+                {
+                    centrPic[i].visible = false;
+                }
+                char buff[50];
+                ifstream filein(FileName); // окрываем файл для чтения
+                while (filein.good())
+                {
+                    filein.getline(buff, 50);
+                    int x = atoi(buff);
+
+                    filein.getline(buff, 50);
+                    int y = atoi(buff);
+
+                    filein.getline(buff, 50);
+                    string adress = buff;
+
+                    filein.getline(buff, 50);
+                    int w_scr = atoi(buff);
+
+                    filein.getline(buff, 50);
+                    int h_scr = atoi(buff);
+
+                    for(int i = 0; i<count_pic; i++)
+                    {
+                        if(menuPic[i].adress == adress)
+                        {
+                          centrPic[nCentralPic] = { x,
+                                                    y,
+                                                    menuPic[i].adress,
+                                                    menuPic[i].pic,
+                                                    w_scr,
+                                                    h_scr,
+                                                    menuPic[i].w,
+                                                    menuPic[i].h,
+                                                    true,
+                                                    menuPic[i].category};
+                          nCentralPic ++;
+                        }
+
+                    }
+
+
+
+                }
+                filein.close();
+            }
+
+            if(btn[btn_help].click())
+            {
+                page = page_help;
+                btn[btn_help].name = "Вернуться";
+                txSleep(100);
+            }
+
+
+
+
+        }
+
+        //справка
+        else if(page == page_help)
+        {
+            txSetColor(TX_BLACK);
+            txSelectFont("Times New Roman", 42);
+            txDrawText(0, 0, 1200, 700,
+                         "Программа Гардероб \n"
+                         "К твоему выбору предоставляется несколько удивительних звёзд, которых ты можешь одевать в разнообразные одевания\n"
+                         "Управление происходит двумя способами: А. Стрелками, Б. Мышкой. \n"
+                         "Аксесуары можно как увеличит, так и уменьшить на кнопки + и - соответственно\n"
+                         "Сохранение и загрузка твоих художеств происходят по отдельных кнопках в нижнем левом углу \n"
+                         "Вы можете сделать снимок ваших художеств, поместив их в окно и нажав одноимённую кнопку. \n"
+                         "Выход на ESC. \n");
+
+            btn[btn_help].draw();
+            if(btn[btn_help].click())
+            {
+                page = page_redactor;
+                btn[btn_help].name = "Справка";
+                txSleep(100);
+            }
+
+
+        }
+
+        if(btn[btn_prntsc].click())
+        {
+            ScreenCapture(250, 100, 700, 500, "1.bmp", txWindow());
+            txMessageBox("Сохранено в 1.bmp");
         }
 
 
 
-//Win32::TransparentBlt (txDC(), 350, 200, 500, 500, pictures, 0, 0, 500, 500, TX_WHITE);
         txEnd();
         txSleep(50);
     }
